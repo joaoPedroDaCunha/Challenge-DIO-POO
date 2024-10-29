9acc86623ebd8f2806cc8632d54c96249cca482a
JoaoPedroDaCunha <joaopedrodacunha.santos@gmail.com>
2024-10-29 11:46:25 -0300

    variables , gettes, settes class Mentoring

--- src/br/com/dio/challenge/doman/Mentoring.java
+++ src/br/com/dio/challenge/doman/Mentoring.java
@@ -1,5 +1,52 @@
 package br.com.dio.challenge.doman;
 
+import java.time.LocalDate;
+
 public class Mentoring {
+    
+    private String title;
+    private String discription;
+    private LocalDate date;
+    
+    public Mentoring(String title, String discription, LocalDate date) {
+        this.title = title;
+        this.discription = discription;
+        this.date = date;
+    }
+
+    public Mentoring(String title, LocalDate date) {
+        this.title = title;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDiscription() {
+        return discription;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoring [title=" + title + ", discription=" + discription + ", date=" + date + "]";
+    }
+
     
 }
