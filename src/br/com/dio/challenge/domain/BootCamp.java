0496bb2355f2732804e4df66a8b2fad2a2a3e7f5
JoaoPedroDaCunha <joaopedrodacunha.santos@gmail.com>
2024-10-29 12:43:23 -0300

    variables, gettes, settes , equals() hastcode()

--- src/br/com/dio/challenge/domain/BootCamp.java
+++ src/br/com/dio/challenge/domain/BootCamp.java
@@ -1,5 +1,17 @@
 package br.com.dio.challenge.domain;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class BootCamp {
+
+    private String name;
+    private String discipition;
+    private final LocalDate initDate = LocalDate.now();
+    private final LocalDate finalDate = initDate.plusDays(45);
+    private Set<Dev> submitDevs = new HashSet<>();
+    private Set<Content> contents = new LinkedHashSet<>();
     
 }
--- src/br/com/dio/challenge/domain/Dev.java
+++ src/br/com/dio/challenge/domain/Dev.java
@@ -1,5 +1,20 @@
 package br.com.dio.challenge.domain;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Dev {
     
+    private String name;
+    private Set<Content> subscribedContent = new LinkedHashSet<>();
+    private Set<Content> completedContent = new LinkedHashSet<>();
+
+    public void submitBootCamp(BootCamp bootcamp){}
+
+    public void progress(){}
+
+    public void calculeteTotalXP(){}
+
+    
+
 }
