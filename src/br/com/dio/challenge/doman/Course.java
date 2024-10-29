6349d063106342b3652f7ddd625357a769687a77
JoaoPedroDaCunha <joaopedrodacunha.santos@gmail.com>
2024-10-29 11:43:06 -0300

    variables , gettes, settes course

--- src/br/com/dio/challenge/doman/Course.java
+++ src/br/com/dio/challenge/doman/Course.java
@@ -1,5 +1,51 @@
 package br.com.dio.challenge.doman;
 
+
 public class Course {
+
+    private String title;
+    private String description;
+    private int workload;
+    
+    public Course(String title, String description, int workload) {
+        this.title = title;
+        this.description = description;
+        this.workload = workload;
+    }
+
+    public Course(String title, int workload) {
+        this.title = title;
+        this.workload = workload;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getWorkload() {
+        return workload;
+    }
+
+    public void setWorkload(int workload) {
+        this.workload = workload;
+    }
+
+    @Override
+    public String toString() {
+        return "[title=" + title + ", description=" + description + ", workload=" + workload + "]";
+    }
+    
     
 }
